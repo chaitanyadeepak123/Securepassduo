ee87548946da040cb6de831a2ca22cdca8b50dbb
chaitanyadeepak123 <gudimetlachaitanyadeepak@gmail.com>
2026-07-09 21:27:24 +0530

    Refactor UART1 ISR and related functions
    Refactor UART1 interrupt service routine and related functions for clarity and efficiency. Adjust variable declarations and comments for better understanding.

--- uart_interuppt1.c
+++ uart_interuppt1.c
@@ -1,284 +1,247 @@
-#include <LPC214x.H>  /* LPC21xx definitions         */
-#include"uart_interuppt.h"
+#include <LPC214x.H>          /* LPC2148 Register Definitions */
+#include "uart_interuppt.h"
 #include <string.h>
-#include<time.h>
-#include"lcd.h"
+#include <time.h>
+#include "lcd.h"
 #include "rtc.h"
-#define UART_INT_ENABLE 1
- 
-char buff1[200],dummy1;
 
-unsigned char i1=0,ch1,r_flag1;
-void InitUART1 (void);
+#define UART_INT_ENABLE 1     /* Enable UART1 Interrupt */
 
+/*---------------------------------------------------------
+ Global Variables
+----------------------------------------------------------*/
+
+/* UART1 Receive Buffer */
+char buff1[200];
+
+/* Dummy variable used to clear interrupt */
+char dummy1;
+
+/* UART1 Variables */
+unsigned char i1 = 0;         /* Receive Buffer Index */
+unsigned char ch1;            /* Received Character */
+unsigned char r_flag1;        /* RFID Reception Status Flag */
+
+
+/*---------------------------------------------------------
+ Function Name : UART1_isr()
+
+ Description   : UART1 Interrupt Service Routine.
+                 Receives RFID tag data from UART1 and stores
+                 it in the receive buffer. The RFID frame
+                 starts with STX (0x02) and ends with ETX
+                 (0x03).
+
+                 r_flag1 Status:
+                 0 -> Waiting for RFID
+                 1 -> Receiving RFID Data
+                 2 -> RFID Reception Completed
+----------------------------------------------------------*/
 void UART1_isr(void) __irq
-
 {
+    /* Check Receive Interrupt */
+    if(U1IIR & 0x04)
+    {
+        /* Read Received Character */
+        ch1 = U1RBR;
 
-  if((U1IIR & 0x04)) //check if receive interrupt
+        /* Start of RFID Frame (STX) */
+        if(ch1 == 0x02)
+        {
+            r_flag1 = 1;
+        }
 
-  {
+        /* Store RFID Data */
+        else if((ch1 != 0x03) && (r_flag1 == 1))
+        {
+            buff1[i1++] = ch1;
+        }
 
-		ch1= U1RBR;
-		if(ch1==0x02)
-		{	/* Read to Clear Receive Interrupt */
+        /* End of RFID Frame (ETX) */
+        else
+        {
+            buff1[i1] = '\0';
 
-			//buff1[i1++] = ch1; 
-			r_flag1=1;
-		/*if(i>=8)
+            i1 = 0;
 
-		{
+            r_flag1 = 2;
+        }
+    }
+    else
+    {
+        /* Clear Transmit Interrupt */
+        dummy1 = U1IIR;
+    }
 
-			tag[8] = '\0';
-
-			i=0;
-
-			//r_flag = 1;
-
-		} */
-		}
-		else if((ch1!=0x03)&&(r_flag1==1))
-		{
-		buff1[i1++]=ch1;
-		}
-		else 
-		{
-		 buff1[i1]='\0';
-		 i1=0;
-		 r_flag1=2;
-		}
-
-  }
-
-  else
-
-  {
-
-      dummy1=U1IIR; //Read to Clear transmit interrupt
-
-  
-
-  }
-
-   VICVectAddr = 0; /* dummy write */
-
+    /* Notify VIC that ISR execution is complete */
+    VICVectAddr = 0;
 }
 
 
-void InitUART1 (void) /* Initialize Serial Interface       */ 
+/*---------------------------------------------------------
+ Function Name : InitUART1()
 
-{  
+ Description   : Initializes UART1 for communication with
+                 the RFID Reader.
 
-            		
+ Configuration:
+     • Baud Rate : 9600
+     • Data Bits : 8
+     • Stop Bits : 1
+     • Parity    : None
+     • Interrupt : Enabled
+----------------------------------------------------------*/
+void InitUART1(void)
+{
+    /* Configure UART1 TXD1 and RXD1 Pins */
+    PINSEL0 |= 0x00050000;
 
-  PINSEL0 |= 0x00050000; /* Enable RxD0 and TxD0              */
+    /* 8-bit Data, No Parity, 1 Stop Bit */
+    U1LCR = 0x83;
 
-  U1LCR = 0x83;         /* 8 bits, no Parity, 1 Stop bit     */
+    /* Baud Rate = 9600 */
+    U1DLL = 97;
 
-  U1DLL = 97;           /* 9600 Baud Rate @ CCLK/4 VPB Clock  */
+    /* Disable Divisor Latch Access */
+    U1LCR = 0x03;
 
-  U1LCR = 0x03;         /* DLAB = 0  */
+#if UART_INT_ENABLE > 0
 
-  
+    /* Configure UART1 Interrupt */
+    VICIntSelect = 0x00000000;
 
-  #if UART_INT_ENABLE > 0
+    VICVectAddr1 = (unsigned)UART1_isr;
 
+    VICVectCntl1 = 0x20 | 7;
 
-  VICIntSelect = 0x00000000; // IRQ
+    VICIntEnable |= (1 << 7);
 
-  VICVectAddr1 = (unsigned)UART1_isr;
+    /* Enable Receive and THRE Interrupts */
+    U1IER = 0x03;
 
-  VICVectCntl1 = 0x20 | 7; /* UART0 Interrupt */
-
-  VICIntEnable |= 1 << 7; 
-
-  
-
-  
-
-  /* Enable UART0 Interrupt */
-
- 
-
- // U0IIR = 0xc0;
-
- // U0FCR = 0xc7;
-
-  U1IER = 0x03;       /* Enable UART0 RX and THRE Interrupts */   
-
-             
-
-  #endif
-
-						
-
+#endif
 }
 
 
-void UART1_Tx(char ch)  /* Write character to Serial Port    */  
+/*---------------------------------------------------------
+ Function Name : UART1_Tx()
 
-{ 
+ Description   : Transmits a single character through UART1.
 
-  while (!(U1LSR & 0x20));
+ Input :
+      ch - Character to transmit.
+----------------------------------------------------------*/
+void UART1_Tx(char ch)
+{
+    /* Wait until transmitter is ready */
+    while(!(U1LSR & 0x20));
 
-  U1THR = ch;                
-
+    /* Send Character */
+    U1THR = ch;
 }
 
 
-char UART1_Rx(void)    /* Read character from Serial Port   */
+/*---------------------------------------------------------
+ Function Name : UART1_Rx()
 
-{                     
+ Description   : Receives a single character from UART1.
 
-  while (!(U1LSR & 0x01));
+ Return :
+      Received Character
+----------------------------------------------------------*/
+char UART1_Rx(void)
+{
+    /* Wait until data is received */
+    while(!(U1LSR & 0x01));
 
-  return (U1RBR);
-
+    return U1RBR;
 }
 
 
+/*---------------------------------------------------------
+ Function Name : UART1_Str()
 
+ Description   : Sends a null-terminated string through
+                 UART1.
 
-
+ Input :
+      s - Pointer to string.
+----------------------------------------------------------*/
 void UART1_Str(char *s)
-
 {
-
-   while(*s)
-
-       UART1_Tx(*s++);
-
+    while(*s)
+    {
+        UART1_Tx(*s++);
+    }
 }
 
 
+/*---------------------------------------------------------
+ Function Name : UART1_Int()
+
+ Description   : Sends an unsigned integer through UART1.
+
+ Input :
+      n - Integer to transmit.
+----------------------------------------------------------*/
 void UART1_Int(unsigned int n)
-
 {
+    unsigned char a[10] = {0};
 
-  unsigned char a[10]={0,0,0,0,0,0,0,0,0,0};
+    int i = 0;
 
-  int i=0;
+    if(n == 0)
+    {
+        UART1_Tx('0');
 
-  if(n==0)
+        return;
+    }
 
-  {
+    while(n > 0)
+    {
+        a[i++] = (n % 10) + '0';
 
-    UART0_Tx('0');
+        n /= 10;
+    }
 
-	return;
-
-  }
-
-  else
-
-  {
-
-     while(n>0)
-
-	 {
-
-	   a[i++]=(n%10)+48;
-
-	   n=n/10;
-
-	 }
-
-	 --i;
-
-	 for(;i>=0;i--)
-
-	 {
-
-	   UART1_Tx(a[i]);
-
-	 }
-
-   }
-
+    while(--i >= 0)
+    {
+        UART1_Tx(a[i]);
+    }
 }
 
 
+/*---------------------------------------------------------
+ Function Name : UART1_Float()
+
+ Description   : Sends a floating-point number through
+                 UART1.
+
+ Input :
+      f - Floating-point value.
+----------------------------------------------------------*/
 void UART1_Float(float f)
-
 {
+    int x;
 
-  int x;
+    float temp;
 
-  float temp;
+    x = f;
 
-  x=f;
+    UART1_Int(x);
 
-  UART1_Int(x);
+    UART1_Tx('.');
 
-  UART1_Tx('.');
+    temp = (f - x) * 100;
 
-  temp=(f-x)*100;
+    x = temp;
 
-  x=temp;
-
-  UART1_Int(x);
-
+    UART1_Int(x);
 }
 
 
-
-/*int main()
-
-{
-	s32 hour,min,sec;
-	char otp[5],tag1[10]="12527137";
-   int h1,h2,m1,m2,s1,s2; 
-	 InitUART0();
-	 //InitUART1();
-  InitLCD();
-  StrLCD("scan Card");
-
-					
-  while(1)
-
-  {
-
-		i=0;r_flag=0;
-	  UART0_Str("RFID\r\n");
-    while(r_flag==0);
-	UART0_Str("");
-	 CmdLCD(0x01);
-	 StrLCD("RFID TAG:");
-	 CmdLCD(0xc0);
-	 StrLCD(buff);
-	 delay_ms(1000);
-	CmdLCD(0x01);
-	GetRTCTimeInfo(&hour,&min,&sec);
-	SetRTCTimeInfo(7,34,55); 
-    h1=hour/10;
-    h2=hour%10;
-    m1=min/10;
-    m2=min%10;
-    s1=sec/10;
-    s2=sec%10;
-	otp[0]=(((h1+s2))*3%10)+'0';
-	otp[1]=(((m2+h2*s1)*7)%10)+'0';
-	otp[2]=(((h1*m2+s2)*11)%10)+'0';
-	otp[3]=(((h2+m2+s1+s2)*13)%10)+'0';
-  	otp[4]='\0'; 
-   if(strcmp(buff,tag1)==0)
-   {
-      CmdLCD(0x80);
-      StrLCD(otp);
-   	  CmdLCD(0xc0);
-	  StrLCD("Acesss Granted");
-	 
-   }
-   else 
-   {
-   	  
-	  CmdLCD(0xc0);
-	  StrLCD("Access Denied");
-   }
-	  r_flag=0;
-
-  }
-
-}  */
-
-
+/**************************************************************
+ The code below is a sample test program (currently commented)
+ demonstrating RFID tag reading and OTP generation. It is
+ intended only for development and testing purposes.
+**************************************************************/
