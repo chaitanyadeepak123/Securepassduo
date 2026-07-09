a2a0a35a350ca14990f713c0222c89942db21288
chaitanyadeepak123 <gudimetlachaitanyadeepak@gmail.com>
2026-07-09 21:14:21 +0530

    Refactor RTC clock configuration and functions

--- rtc.c
+++ rtc.c
@@ -4,142 +4,345 @@
 #include "lcd_defines.h"
 #include "rtc.h"
 
-#define FOSC 12000000
-#define CCLK (5*FOSC)
-#define PCLK (CCLK/4)
+/*---------------------------------------------------------
+ System Clock Configuration
+----------------------------------------------------------*/
 
-#define PREINT_VAL (int)((PCLK/32768)-1)
-#define PREFRAC_VAL (PCLK-((PREINT_VAL+1)*32768))
+/* Oscillator Frequency (12 MHz Crystal) */
+#define FOSC            12000000
 
-#define RTC_ENABLE (1<<0)
-#define RTC_RESET  (1<<1)
-#define RTC_CLKSRC (1<<4)
+/* CPU Clock Frequency */
+#define CCLK            (5 * FOSC)
 
-#define SUN 0
-#define MON 1
-#define TUE 2
-#define WED 3
-#define THU 4
-#define FRI 5
-#define SAT 6
+/* Peripheral Clock Frequency */
+#define PCLK            (CCLK / 4)
 
+/* RTC Prescaler Integer Value */
+#define PREINT_VAL      (int)((PCLK / 32768) - 1)
+
+/* RTC Prescaler Fractional Value */
+#define PREFRAC_VAL     (PCLK - ((PREINT_VAL + 1) * 32768))
+
+
+/*---------------------------------------------------------
+ RTC Control Register Bit Definitions
+----------------------------------------------------------*/
+
+/* Enable RTC Counter */
+#define RTC_ENABLE      (1 << 0)
+
+/* Reset RTC Counter */
+#define RTC_RESET       (1 << 1)
+
+/* Select External RTC Clock Source */
+#define RTC_CLKSRC      (1 << 4)
+
+
+/*---------------------------------------------------------
+ Day of Week Definitions
+----------------------------------------------------------*/
+
+#define SUN     0
+#define MON     1
+#define TUE     2
+#define WED     3
+#define THU     4
+#define FRI     5
+#define SAT     6
+
+
+/* Define LPC2148 Device */
 #define _LPC2148
+
+
+/*---------------------------------------------------------
+ Function Name : RTC_Init()
+
+ Description   : Initializes the RTC module by enabling
+                 the RTC clock and configuring the
+                 prescaler values if required.
+----------------------------------------------------------*/
 void RTC_Init(void)
 {
-      CCR = RTC_RESET;
-     #ifndef _LPC2148
-     PREINT = PREINT_VAL;
-     PREFRAC = PREFRAC_VAL;
-      CCR = RTC_ENABLE;
-        #else
-         CCR = RTC_ENABLE  | RTC_CLKSRC;
-        #endif
+    /* Reset RTC */
+    CCR = RTC_RESET;
+
+#ifndef _LPC2148
+
+    /* Configure RTC Prescalers */
+    PREINT = PREINT_VAL;
+    PREFRAC = PREFRAC_VAL;
+
+    /* Enable RTC */
+    CCR = RTC_ENABLE;
+
+#else
+
+    /* Enable RTC using external clock source */
+    CCR = RTC_ENABLE | RTC_CLKSRC;
+
+#endif
 }
 
-char week[][4]={"SUN","MON","TUE","WED","THU","FRI","SAT"};
-void GetRTCTimeInfo(s32 *hour,s32 *min,s32 *sec)
+
+/*---------------------------------------------------------
+ Weekday String Table
+----------------------------------------------------------*/
+char week[][4] =
+{
+    "SUN",
+    "MON",
+    "TUE",
+    "WED",
+    "THU",
+    "FRI",
+    "SAT"
+};
+
+
+/*---------------------------------------------------------
+ Function Name : GetRTCTimeInfo()
+
+ Description   : Reads the current RTC time.
+
+ Input         :
+      hour - Pointer to hour variable
+      min  - Pointer to minute variable
+      sec  - Pointer to second variable
+----------------------------------------------------------*/
+void GetRTCTimeInfo(s32 *hour, s32 *min, s32 *sec)
 {
     *hour = HOUR;
-    *min = MIN;
-    *sec = SEC;
+    *min  = MIN;
+    *sec  = SEC;
 }
 
-void DisplayRTCTime(u32 hour,u32 min,u32 sec)
+
+/*---------------------------------------------------------
+ Function Name : DisplayRTCTime()
+
+ Description   : Displays the current time on the LCD
+                 in HH:MM:SS format.
+
+ Input :
+      hour, min, sec
+----------------------------------------------------------*/
+void DisplayRTCTime(u32 hour, u32 min, u32 sec)
 {
     CmdLCD(GOTO_LINE1_POS0);
 
-    CharLCD(hour/10+48);
-    CharLCD(hour%10+48);
+    CharLCD(hour/10 + '0');
+    CharLCD(hour%10 + '0');
     CharLCD(':');
 
-    CharLCD(min/10+48);
-    CharLCD(min%10+48);
+    CharLCD(min/10 + '0');
+    CharLCD(min%10 + '0');
     CharLCD(':');
 
-    CharLCD(sec/10+48);
-    CharLCD(sec%10+48);
+    CharLCD(sec/10 + '0');
+    CharLCD(sec%10 + '0');
 }
 
-void GetRTCDateInfo(s32 *date,s32 *month,s32 *year)
+
+/*---------------------------------------------------------
+ Function Name : GetRTCDateInfo()
+
+ Description   : Reads the current RTC date.
+
+ Input :
+      date
+      month
+      year
+----------------------------------------------------------*/
+void GetRTCDateInfo(s32 *date, s32 *month, s32 *year)
 {
-    *date = DOM;
+    *date  = DOM;
     *month = MONTH;
-    *year = YEAR;
+    *year  = YEAR;
 }
 
-void DisplayRTCDate(u32 date,u32 month,u32 year)
+
+/*---------------------------------------------------------
+ Function Name : DisplayRTCDate()
+
+ Description   : Displays the current date on the LCD
+                 in DD/MM/YYYY format.
+
+ Input :
+      date
+      month
+      year
+----------------------------------------------------------*/
+void DisplayRTCDate(u32 date, u32 month, u32 year)
 {
     CmdLCD(GOTO_LINE2_POS0);
 
-    CharLCD(date/10+48);
-    CharLCD(date%10+48);
+    CharLCD(date/10 + '0');
+    CharLCD(date%10 + '0');
+
     CharLCD('/');
-    CharLCD(month/10+48);
-    CharLCD(month%10+48);
+
+    CharLCD(month/10 + '0');
+    CharLCD(month%10 + '0');
+
     CharLCD('/');
 
     U32LCD(year);
 }
 
-void SetRTCTimeInfo(u32 hour,u32 min,u32 sec)
+
+/*---------------------------------------------------------
+ Function Name : SetRTCTimeInfo()
+
+ Description   : Sets Hour, Minute and Second registers.
+
+ Input :
+      hour
+      min
+      sec
+----------------------------------------------------------*/
+void SetRTCTimeInfo(u32 hour, u32 min, u32 sec)
 {
     HOUR = hour;
-    MIN = min;
-    SEC = sec;
+    MIN  = min;
+    SEC  = sec;
 }
 
-void SetRTCDateInfo(u32 date,u32 month,u32 year)
+
+/*---------------------------------------------------------
+ Function Name : SetRTCDateInfo()
+
+ Description   : Sets Date, Month and Year registers.
+
+ Input :
+      date
+      month
+      year
+----------------------------------------------------------*/
+void SetRTCDateInfo(u32 date, u32 month, u32 year)
 {
-    DOM = date;
+    DOM   = date;
     MONTH = month;
-    YEAR = year;
+    YEAR  = year;
 }
 
+
+/*---------------------------------------------------------
+ Function Name : GetRTCDay()
+
+ Description   : Reads the current Day of Week.
+
+ Input :
+      dow - Pointer to weekday variable.
+----------------------------------------------------------*/
 void GetRTCDay(s32 *dow)
 {
     *dow = DOW;
 }
 
+
+/*---------------------------------------------------------
+ Function Name : DisplayRTCDay()
+
+ Description   : Displays the day of the week on the LCD.
+
+ Input :
+      dow - Day Number (0-6)
+----------------------------------------------------------*/
 void DisplayRTCDay(u32 dow)
 {
-    CmdLCD(GOTO_LINE1_POS0+10);
+    CmdLCD(GOTO_LINE1_POS0 + 10);
+
     StrLCD(week[dow]);
 }
 
+
+/*---------------------------------------------------------
+ Function Name : SetRTCDay()
+
+ Description   : Updates the RTC Day of Week register.
+
+ Input :
+      dow - Day Number (0-6)
+----------------------------------------------------------*/
 void SetRTCDay(u32 dow)
 {
     DOW = dow;
 }
 
+
+/*---------------------------------------------------------
+ Function Name : sethour()
+
+ Description   : Updates the RTC Hour register.
+----------------------------------------------------------*/
 void sethour(u32 hour)
 {
-	HOUR=hour;
+    HOUR = hour;
 }
 
+
+/*---------------------------------------------------------
+ Function Name : setminute()
+
+ Description   : Updates the RTC Minute register.
+----------------------------------------------------------*/
 void setminute(u32 minute)
 {
-	MIN=minute;
+    MIN = minute;
 }
+
+
+/*---------------------------------------------------------
+ Function Name : setsecond()
+
+ Description   : Updates the RTC Second register.
+----------------------------------------------------------*/
 void setsecond(u32 second)
 {
-	SEC=second;
+    SEC = second;
 }
+
+
+/*---------------------------------------------------------
+ Function Name : setdate()
+
+ Description   : Updates the RTC Date register.
+----------------------------------------------------------*/
 void setdate(u32 date)
 {
-	DOM=date;
+    DOM = date;
 }
+
+
+/*---------------------------------------------------------
+ Function Name : setmonth()
+
+ Description   : Updates the RTC Month register.
+----------------------------------------------------------*/
 void setmonth(u32 month)
 {
-	MONTH=month;
+    MONTH = month;
 }
+
+
+/*---------------------------------------------------------
+ Function Name : setweek()
+
+ Description   : Updates the RTC Day of Week register.
+----------------------------------------------------------*/
 void setweek(u32 week)
 {
-   DOW=week;
-}	
-void setyear(u32 year)
-{
-YEAR=year;
+    DOW = week;
 }
 
 
+/*---------------------------------------------------------
+ Function Name : setyear()
 
+ Description   : Updates the RTC Year register.
+----------------------------------------------------------*/
+void setyear(u32 year)
+{
+    YEAR = year;
+}
