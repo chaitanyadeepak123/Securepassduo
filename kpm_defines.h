b29442b921c093c731236c00136e22081c51ae32
chaitanyadeepak123 <gudimetlachaitanyadeepak@gmail.com>
2026-07-09 21:04:57 +0530

    Update kpm_defines.h

--- kpm_defines.h
+++ kpm_defines.h
@@ -1,9 +1,26 @@
-#define ROW0 16
-#define ROW1 17
-#define ROW2 18
-#define ROW3 19
+/* Row 0 connected to Port 1 Pin 16 */
+#define ROW0    16
 
-#define COL0 20
-#define COL1 21
-#define COL2 22
-#define COL3 23
+/* Row 1 connected to Port 1 Pin 17 */
+#define ROW1    17
+
+/* Row 2 connected to Port 1 Pin 18 */
+#define ROW2    18
+
+/* Row 3 connected to Port 1 Pin 19 */
+#define ROW3    19
+
+
+/*-------------- Keypad Column Pins --------------*/
+
+/* Column 0 connected to Port 1 Pin 20 */
+#define COL0    20
+
+/* Column 1 connected to Port 1 Pin 21 */
+#define COL1    21
+
+/* Column 2 connected to Port 1 Pin 22 */
+#define COL2    22
+
+/* Column 3 connected to Port 1 Pin 23 */
+#define COL3    23
