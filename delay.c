5147d7b2d28979e6e49155e45a6b8fc2fc3d7e8c
chaitanyadeepak123 <gudimetlachaitanyadeepak@gmail.com>
2026-07-09 20:48:38 +0530

    Refactor delay functions for clarity and consistency

--- delay.c
+++ delay.c
@@ -1,34 +1,51 @@
-#include"types.h"
+#include "types.h"
 
+/*---------------------------------------------------------
+ Function Name : delay_us()
+ Description   : Generates an approximate delay in
+                 microseconds using a software delay loop.
+ Input         : tdly - Delay time in microseconds.
+ Return        : None
+----------------------------------------------------------*/
 void delay_us(u32 tdly)
-
 {
+    /* Adjust loop count for 1 microsecond delay */
+    tdly *= 12;
 
-	tdly*=12;
-
-	while(tdly--);
-
+    /* Execute empty loop to create delay */
+    while(tdly--);
 }
 
+
+/*---------------------------------------------------------
+ Function Name : delay_ms()
+ Description   : Generates an approximate delay in
+                 milliseconds using a software delay loop.
+ Input         : tdly - Delay time in milliseconds.
+ Return        : None
+----------------------------------------------------------*/
 void delay_ms(u32 tdly)
-
 {
+    /* Adjust loop count for 1 millisecond delay */
+    tdly *= 12000;
 
-	tdly*=12000;
-
-	while(tdly--);
-
-	
-
+    /* Execute empty loop to create delay */
+    while(tdly--);
 }
 
+
+/*---------------------------------------------------------
+ Function Name : delay_s()
+ Description   : Generates an approximate delay in
+                 seconds using a software delay loop.
+ Input         : tdly - Delay time in seconds.
+ Return        : None
+----------------------------------------------------------*/
 void delay_s(u32 tdly)
-
 {
+    /* Adjust loop count for 1 second delay */
+    tdly *= 12000000;
 
-	tdly*=12000000;
-
-	while(tdly--);
-
-
+    /* Execute empty loop to create delay */
+    while(tdly--);
 }
